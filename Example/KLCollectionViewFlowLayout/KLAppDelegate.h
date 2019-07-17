47932d759f8bd1108102ccdf7d40ff5e1a381e04
Kalan <Kalanhall@163.com>
2019-07-17 10:55:27 +0800

    Initial commit

---
+++ Example/KLCollectionViewFlowLayout/KLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KLAppDelegate.h
+//  KLCollectionViewFlowLayout
+//
+//  Created by 574068650@qq.com on 07/17/2019.
+//  Copyright (c) 2019 574068650@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/KLCollectionViewFlowLayout/KLViewController.h
@@ -0,0 +1,13 @@
+//
+//  KLViewController.h
+//  KLCollectionViewFlowLayout
+//
+//  Created by 574068650@qq.com on 07/17/2019.
+//  Copyright (c) 2019 574068650@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KLViewController : UIViewController
+
+@end
